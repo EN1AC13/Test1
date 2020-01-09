4e9ff4824b1e865c8715984e8e659919b0b3f4be
EN1AC13 <853883507@qq.com>
2020-01-09 16:22:12 +0800

    util

--- hello/epoll.cpp
+++ hello/epoll.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 #include <stack>
 #include <string>
-#include <map>
-#include "hello.h"
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include "util.h"
 using namespace std;
 #define _FD_SETSIZE 1024
 int main() {
+    tk_conf_t conf;
+    cout<<read_conf("./conf.txt",&conf)<<endl;
+    cout<<conf.root<<endl;
+    cout<<conf.port<<conf.thread_num<<endl;
 }
